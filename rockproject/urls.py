6b56eea38a55b8abd1fc7f2f8591aa307377a3cc
Gary Cason <garrettcason@skiff.com>
2024-06-13 14:44:14 -0500

    added rock views

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rockapi.views import register_user, login_user, TypeView
+from rockapi.views import register_user, login_user, TypeView,RockView
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'types', TypeView, 'type')
+router.register(r'types', TypeView, 'ty')
+router.register(r'rocks', RockView,'rock')
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),# Enables http://localhost:8000/register
